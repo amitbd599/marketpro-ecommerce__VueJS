3539662ee0a283a6f6781c6dffb3ea33df277adb
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2025-04-05 11:33:28 +0600

    Initial commit

---
+++ master/src/helper/Preloader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-if="active" class="preloader">
+    <img src="/assets/images/icon/preloader.gif" alt="Preloader" />
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+
+const active = ref(true);
+
+onMounted(() => {
+  setTimeout(() => {
+    active.value = false;
+  }, 500);
+});
+</script>
---
+++ master/src/pages/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <Preloader />
+</template>
+
+<script setup>
+import Preloader from "../helper/Preloader.vue";
+</script>
